6a8675a2ee2b1623582c8a7df9e62b716c038356
Angel Canales <anggar85@gmail.com>
2019-09-29 01:28:30 -0700

    Se agregan animaciones y se crean actividad para lo spins

---
+++ app/src/main/java/com/mxlapps/app/gearspopguide/Views/PinListActivity.java
@@ -0,0 +1,13 @@
+package com.mxlapps.app.gearspopguide.Views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PinListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pin_list);
+    }
+}
